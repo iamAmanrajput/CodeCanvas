b1b80d51cec96e754bf14585d1010346d49204bb
Aman Kumar <aman.it360@gmail.com>
2026-04-07 22:51:16 +0530

    feat: implement authentication setup with better-auth and Prisma adapter

---
+++ src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "./prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  debug: true,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
+});
